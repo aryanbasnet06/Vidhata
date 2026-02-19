df16ea8b93d3f004fbbed23257e9941d2d3b3088
aryanbasnet06 <basnetaryan05@gmail.com>
2026-02-19 13:47:17 +0545

    final website

--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -6,9 +6,9 @@ export function Team() {
     {
       name: "Aryan Basnet",
       position: "Founder & Chief Executive Officer",
-      image: "/images/aryan-basnet.png",
+      image: "/images/aryanbasnet_founder.png",
       description:
-        "Aryan is an emerging engineer and an artist with a strong commitment to social impact. With key expertise in social design, human-centric design, design thinking, and organizational leadership, Aryan spearheads change in the community through being an inventor. Using technology and design thinking as his foothold for innovation, he approaches every problem with a unique 360-degree view that keeps the user at the centre of solution building.",
+        "Aryan is an emerging engineer and social entrepreneur with a strong commitment to social impact. With key expertise in social design, human-centric design, design thinking, and organizational leadership, Aryan spearheads change in the community through being an inventor. Using technology and design thinking as his foothold for innovation, he approaches every problem with a unique 360-degree view that keeps the user at the centre of solution building.",
       instagram: "https://www.instagram.com/__aryanbasnet__/",
       email: "mailto:basnetaryan05@gmail.com",
     },
